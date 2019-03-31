512950be40269191a1028e072f5afe90c1dcff9a
DeepakKPrakash <37238860+DeepakKPrakash@users.noreply.github.com>
2019-03-31 10:30:31 +0530

    update

--- js/timeline.js
+++ js/timeline.js
@@ -1,47 +1,14 @@
-/**********************Scroll Animation "START"************************************/
-$(document).ready(function(){
-var $animation_elements = $('.anim');
-var $window = $(window);
+var slideIndex = 0;
+showSlides();
 
-function check_if_in_view() {
-var window_height = $window.height();
-var window_top_position = $window.scrollTop();
-var window_bottom_position = (window_top_position + window_height);
-
-$.each($animation_elements, function() {
-var $element = $(this);
-var element_height = $element.outerHeight();
-var element_top_position = $element.offset().top;
-var element_bottom_position = (element_top_position + element_height);
-
-//check to see if this current container is within viewport
-if ((element_bottom_position >= window_top_position) &&
-(element_top_position <= window_bottom_position)) {
-$element.addClass('animated');
-} else {
-$element.removeClass('animated');
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
-});
-}
-
-$window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll');
-});
-/**********************Scroll Animation "END"************************************/
-
-/**********************Change color of center aligned animated content small Circle  "START"************************************/
-$(document).ready(function(){
-    $(" .debits").hover(function(){
-        $(" .center-right").css("background-color", "#4997cd");
-        }, function(){
-        $(" .center-right").css("background-color", "#fff");
-    }); 
-});
-$(document).ready(function(){
-    $(".credits").hover(function(){
-        $(".center-left").css("background-color", "#4997cd");
-        }, function(){
-        $(".center-left").css("background-color", "#fff");
-    }); 
-});
-/**********************Change color of center aligned animated content small Circle  "END"************************************/
